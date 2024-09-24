c462b17cba39d47a470edc15ba26b25593056a0b
sho_kuriyama <oranger04771@gmail.com>
2024-09-24 12:05:54 +0900

    タイトル設定

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <div>
+      <Helmet>
+        <title>Home</title>
+      </Helmet>
+      <h1>Home Page </h1>
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
 };
 
 const About = () => {
-  return <h1>About Page</h1>;
+  return (
+    <div>
+      <Helmet>
+        <title>About</title>
+      </Helmet>
+      <h1>About Page</h1>
+    </div>
+  );
 };
 
 function App() {
@@ -22,6 +41,7 @@ function App() {
           </li>
         </ul>
       </nav>
+      <p>SPAのGTM発火テスト</p>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
